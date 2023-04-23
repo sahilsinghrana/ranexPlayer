dbf2737fd7a6e751fa9518f6a4dec383d023223a
Sahil Singh Rana <ranasahil98@gmail.com>
2023-04-23 22:37:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-red-700' >
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
