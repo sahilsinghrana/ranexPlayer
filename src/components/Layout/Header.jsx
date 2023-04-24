0729c555f162e78ea5ea84e015afc15997cff171
Sahil Singh Rana <ranasahil98@gmail.com>
2023-04-24 23:19:38 +0530

    updated div to header tag

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <div className="bg-slate-700 py-3">
+    <header className="bg-backgroundDarker py-3">
       <h1 className="text-center text-3xl ">Ranex Player</h1>
-    </div>
+    </header>
   );
 };
 
