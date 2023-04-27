aef492744067c3ce84068a0975290540c321e542
Sahil Singh Rana <ranasahil98@gmail.com>
2023-04-28 00:39:19 +0530

    Added basename

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,25 +2,28 @@ import NotFound from '../pages/NotFound';
 
 import {createBrowserRouter} from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    errorElement: <NotFound />,
-    async lazy() {
-      const Component = await import('../components/Layout');
-      return {Component: Component.default};
-    },
-    children: [
-      {
-        path: '/',
-        async lazy() {
-          const Component = await import('../pages/Home/index.jsx');
-          return {Component: Component.default};
-        },
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      errorElement: <NotFound />,
+      async lazy() {
+        const Component = await import('../components/Layout');
+        return {Component: Component.default};
       },
-    ],
-  },
-]);
+      children: [
+        {
+          path: '/',
+          async lazy() {
+            const Component = await import('../pages/Home/index.jsx');
+            return {Component: Component.default};
+          },
+        },
+      ],
+    },
+  ],
+  {basename: '/ranexPlayer'}
+);
 
 router.displayName = 'Router';
 
