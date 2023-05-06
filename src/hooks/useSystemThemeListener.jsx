572a45d32b85e8e064199921b32dcf5d38be68a5
Sahil Singh Rana <ranasahil98@gmail.com>
2023-05-06 23:31:32 +0530

    fix: themeListner removal

--- src/hooks/useSystemThemeListener.jsx
+++ src/hooks/useSystemThemeListener.jsx
@@ -2,16 +2,30 @@ import {THEME_SELECTOR_OPTIONS, updateThemeClassInDom} from '../lib/theme';
 import themeAtom from '../store/atoms/themeAtom';
 
 import {useAtom} from 'jotai';
+import {useCallback} from 'react';
 import {useLayoutEffect} from 'react';
 
-const removeEventListener = () => {
+const removeEventListener = (func) => {
   window
     .matchMedia('(prefers-color-scheme: dark)')
-    .removeEventListener('change', () => {});
+    .removeEventListener('change', func);
 };
 
 const useSystemThemeListener = () => {
   const [theme, setTheme] = useAtom(themeAtom);
+
+  const handleThemeChange = useCallback(
+    (event) => {
+      const newColorScheme = event.matches ? 'dark' : 'light';
+      setTheme({
+        theme: newColorScheme,
+        selection: THEME_SELECTOR_OPTIONS.system,
+      });
+      updateThemeClassInDom(newColorScheme);
+    },
+    [setTheme]
+  );
+
   useLayoutEffect(() => {
     if (theme.selection === THEME_SELECTOR_OPTIONS.user) {
       removeEventListener();
@@ -19,19 +33,12 @@ const useSystemThemeListener = () => {
     } else {
       window
         .matchMedia('(prefers-color-scheme: dark)')
-        .addEventListener('change', (event) => {
-          const newColorScheme = event.matches ? 'dark' : 'light';
-          setTheme({
-            theme: newColorScheme,
-            selection: THEME_SELECTOR_OPTIONS.system,
-          });
-          updateThemeClassInDom(newColorScheme);
-        });
+        .addEventListener('change', handleThemeChange);
     }
     return () => {
-      removeEventListener();
+      removeEventListener(handleThemeChange);
     };
-  }, [setTheme, theme.selection, theme.theme]);
+  }, [setTheme, theme.selection, theme.theme, handleThemeChange]);
 
   return theme;
 };
