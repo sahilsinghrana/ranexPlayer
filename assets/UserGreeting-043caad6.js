50c4d83edbbbb118842b402e227098ccf125eb05
sahilsinghrana <48375250+sahilsinghrana@users.noreply.github.com>
2024-07-05 12:21:42 +0000

    Deploying to gh-pages from  @ 5e12c1290a6ad6e3893210cfd52b3547e05b6b0b 🚀

---
+++ assets/UserGreeting-043caad6.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-0f92d19b.js";const e=()=>t.jsx("h1",{className:"p-4 font-sans font-extrabold tracking-wide text-1xl text-textPrimary dark:text-textPrimaryDark",children:"Good Morning, User"});e.displayName="UserGreeting";export{e as default};
