d8eafb3b860711cccbd5669c1f5e97cceee6ec59
sahilsinghrana <48375250+sahilsinghrana@users.noreply.github.com>
2024-07-07 18:34:49 +0000

    Deploying to gh-pages from  @ 18627ba17f5615df79d1310274cb871f2019817b 🚀

---
+++ assets/Footer-344245f6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-5a1e1a62.js";const r=()=>e.jsxs("footer",{className:"flex items-center justify-center w-full grid-cols-3 py-2 font-mono text-center border-t-2 text border-borderHover/10 bg-neutral-300 dark:bg-neutral-900 ",children:["With ❣️ by Sahil Rana"," "]});r.displayName="Footer";export{r as default};
--- assets/index-ec1d1275.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ee679bec.js";function a(){return r.jsx("img",{className:"bg-white dark:bg-black h-14 w-14",href:"/",alt:"cover art"})}export{a as A};
