042c5ad88eb2d8b12a1b65800b630d1baecb777a
sahilsinghrana <48375250+sahilsinghrana@users.noreply.github.com>
2024-07-08 18:10:23 +0000

    Deploying to gh-pages from  @ d810dcbb481e6b94f800f79b7c052634240a7d76 🚀

---
+++ assets/Logo-b68e6201.js
@@ -0,0 +1,1 @@
+import{j as o,L as r}from"./index-4940ecbb.js";import{l as a}from"./logo_red-54b98162.js";import{c as t}from"./clsx.m-1229b3e0.js";const x=({className:s})=>o.jsx(r,{to:"/",children:o.jsx("img",{className:t("h-[50px]",s),src:a,alt:"Ranex Player Logo"})});export{x as default};
