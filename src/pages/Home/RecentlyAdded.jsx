a81fcd2f68e5b70be9a98dd41fca4451eef54c02
sahilsinghrana <ranasahil98@gmail.com>
2024-07-20 19:08:19 +0530

    admin and public songs

--- src/pages/Home/RecentlyAdded.jsx
+++ src/pages/Home/RecentlyAdded.jsx
@@ -3,6 +3,7 @@ import HomeSectionLoader from "../../components/Loaders/HomeSectionLoader";
 import {lazyWithRetry} from "../../utils/reactLazy";
 
 import {Suspense} from "react";
+import useSWR from "swr";
 
 const HomeSection = lazyWithRetry(() => import("./HomeSection"));
 
@@ -11,13 +12,22 @@ const HorizontalList = lazyWithRetry(() =>
 );
 
 function RecentlyAdded() {
+  const {data} = useSWR("/music/song");
+
   return (
     <Suspense fallback={<HomeSectionLoader />}>
       <HomeSection title="Celestial catalog!">
         <HorizontalList className="gap-3 mb-2 sm:px-3">
-          <li>
-            <SongCard artist="Europe" title="In my time" />
-          </li>
+          {data.data.songs?.map((song) => {
+            return (
+              <li key={song?.fileName}>
+                <SongCard
+                  artist="work in progress"
+                  title={song?.originalFilename?.split(".")[0]}
+                />
+              </li>
+            );
+          })}
           <li>
             <SongCard artist="Hardwell" title="Spaceman" />
           </li>
