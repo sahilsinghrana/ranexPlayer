ece71b66390971a39a373320c291fb168c4d84a2
Sahil Rana <48375250+sahilsinghrana@users.noreply.github.com>
2024-08-13 13:43:35 +0000

    service worker coverart images cache

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-// import supabase from "../config/supabase";
 
 const baseServerUrl = import.meta.env.VITE_SERVER_URL;
 const accessToken = `Basic ${import.meta.env.VITE_ACCESS_TOKEN}`;
--- src/helpers/fetcher.js
+++ src/helpers/fetcher.js
@@ -1,11 +1,10 @@
 import axiosInstance from "./axiosInstance";
 
 const fetcher = async (url, config = {}) => {
-  axiosInstance.get;
   return axiosInstance
     .get(url, {
       ...config,
-      timeout: 3000,
+      timeout: 55000,
     })
     .then((res) => res.data);
 };
