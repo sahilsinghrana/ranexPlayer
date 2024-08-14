a2462ddadaac3b9cea1e71c0fabdc3ae8ddda32c
Sahil Rana <48375250+sahilsinghrana@users.noreply.github.com>
2024-08-14 07:02:01 +0000

    fix: caching

--- public/sw.js
+++ public/sw.js
@@ -10,7 +10,12 @@ const putInCache = async (request, response) => {
   await cache.put(request, response);
 };
 
-const cacheFirst = async ({request, preloadResponsePromise, fallbackUrl}) => {
+const cacheFirst = async ({request, preloadResponsePromise}) => {
+  const url = new URL(request.url);
+  if (url.origin.includes("coverartarchive")) {
+    request.mode = "no-cors";
+  }
+
   // First try to get the resource from the cache
   const responseFromCache = await caches.match(request);
   if (responseFromCache) {
@@ -32,20 +37,16 @@ const cacheFirst = async ({request, preloadResponsePromise, fallbackUrl}) => {
 
   // Next try to get the resource from the network
   try {
-    const responseFromNetwork = await fetch(request.clone());
+    const req = request.clone();
+
+    const responseFromNetwork = await fetch(req);
     // response may be used only once
     // we need to save clone to put one copy in cache
     // and serve second one
-    putInCache(request, responseFromNetwork.clone());
+    if (responseFromNetwork.ok)
+      putInCache(request, responseFromNetwork.clone());
     return responseFromNetwork;
   } catch (error) {
-    const fallbackResponse = await caches.match(fallbackUrl);
-    if (fallbackResponse) {
-      return fallbackResponse;
-    }
-    // when even the fallback response is not available,
-    // there is nothing we can do, but we must always
-    // return a Response object
     return new Response("Network error happened", {
       status: 408,
       headers: {"Content-Type": "text/plain"},
@@ -69,9 +70,9 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  const url = new URL(event.request.url);
-  // console.log(event.request.url, url);
-  if (url.origin.includes("coverartarchive")) {
+  if (
+    ["coverartarchive", "/image"].some((alt) => event.request.url.includes(alt))
+  ) {
     event.respondWith(
       cacheFirst({
         request: event.request,
