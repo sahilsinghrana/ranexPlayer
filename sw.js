6e42b24ec3c5861de7b2ecc2714aebd6c024674e
sahilsinghrana <48375250+sahilsinghrana@users.noreply.github.com>
2024-08-15 17:32:11 +0000

    Deploying to gh-pages from @ sahilsinghrana/ranexPlayer@069fdc27fd934efc070225ae4bdd359cef45075f 🚀

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/scripts/cacherSw.js?v1.1")
+    .register("/scripts/cacherSw.js?v2")
     .then(function () {})
     .catch(function (_) {});
 }
