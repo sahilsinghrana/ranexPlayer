232b9df9f488fa60af89d2795c25aeb4b039cbb3
Sahil Rana <48375250+sahilsinghrana@users.noreply.github.com>
2024-08-21 07:20:22 +0000

    feat: admin -collapsible sidebar fix: suspense loaders

--- src/components/Admin/SettingsSidebar.jsx
+++ src/components/Admin/SettingsSidebar.jsx
@@ -1,21 +1,71 @@
 import SidebarLink from "./SidebarLink";
 
 import useUserProfile from "../../hooks/fetch/useUserProfile";
+import {useIsMobile} from "../../hooks/useMediaQuery";
+
+import {
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+  GearIcon,
+  Link2Icon,
+  PersonIcon,
+  RocketIcon,
+} from "@radix-ui/react-icons";
+import clsx from "clsx";
+import {useState} from "react";
 
 function SettingsSidebar() {
   const {user} = useUserProfile();
+  const isMobile = useIsMobile();
+
+  const [collapsed, setCollapsed] = useState(isMobile);
+
   return (
     <div
-      className="py-1 min-h-[300px] text-sm md:px-2"
+      className={clsx("py-1 text-sm pr-2 min-h-[300px]", {
+        "": !collapsed,
+      })}
       style={{
         borderRight: "1px solid #ffffff50",
       }}
     >
-      <ul>
-        <SidebarLink to="/settings/profile">Profile</SidebarLink>
-        <SidebarLink to="/settings/account">Account</SidebarLink>
-        <SidebarLink to="/settings/cloudIntegrations">Integrations</SidebarLink>
-        {!!user?.iam && <SidebarLink to="/settings/admin">Admin</SidebarLink>}
+      <button
+        className="flex justify-end w-full p-1 mb-4 text-right rounded-md bg-gray-600/60"
+        onClick={() => setCollapsed((p) => !p)}
+      >
+        {collapsed ? <DoubleArrowRightIcon /> : <DoubleArrowLeftIcon />}
+      </button>
+      <ul className="mt-4 ">
+        <SidebarLink
+          Icon={PersonIcon}
+          collapsed={collapsed}
+          to="/settings/profile"
+        >
+          Profile
+        </SidebarLink>
+        <SidebarLink
+          Icon={GearIcon}
+          collapsed={collapsed}
+          to="/settings/account"
+        >
+          Account
+        </SidebarLink>
+        <SidebarLink
+          Icon={Link2Icon}
+          collapsed={collapsed}
+          to="/settings/cloudIntegrations"
+        >
+          Integrations
+        </SidebarLink>
+        {!!user?.iam && (
+          <SidebarLink
+            Icon={RocketIcon}
+            collapsed={collapsed}
+            to="/settings/admin"
+          >
+            Admin
+          </SidebarLink>
+        )}
       </ul>
     </div>
   );
